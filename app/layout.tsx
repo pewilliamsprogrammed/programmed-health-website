44826a48637476fc8a68c59dc61ffa21cee3450d
Jody Biddle <Jody.Biddle@programmed.com.au>
2024-08-20 08:32:01 +1000

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,9 +21,26 @@ const fontBody = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Programmed health professionals",
+  title: "Programmed Health | No 1 Nursing Agency in Australia",
   description:
-    "Australia's busiest nursing agency is recruiting registered nurses, enrolled nurses, personal care attendants, personal service workers leading healthcare recruitment agency for imediated start. Work across Austalia Melbourne, Sydney, Perth, Brisbane, and Adelaide.",
+    "Australia's busiest nursing agency is recruiting registered nurses, enrolled nurses, personal care attendants, personal service workers. Australias leading healthcare recruitment agency need nurse now for imediated start. Nursing agency in Austalia Melbourne, Sydney, Perth, Brisbane, and Adelaide.",
+  keywords:
+    "nursing agency, healthcare recruitment agency, nursing agency in Australia, nursing agency in Melbourne, nursing agency in Sydney, nursing agency in Perth, nursing agency in Brisbane, nursing agency in Adelaide, healthcare recruitment agency",
+  openGraph: {
+    title: "Programmed Health | No 1 Nursing Agency in Australia",
+    description:
+      "Australia's busiest nursing agency is recruiting registered nurses, enrolled nurses, personal care attendants, personal service workers. Australias leading healthcare recruitment agency need nurse now for imediated start. Nursing agency in Austalia Melbourne, Sydney, Perth, Brisbane, and Adelaide.",
+    type: "website",
+    url: "https://programmedhealth.com.au",
+    images: [
+      {
+        url: "https://programmedhealth.com.au/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Programmed Health | No 1 Nursing Agency in Australia",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
