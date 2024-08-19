ec479bad62d966bc728546f84d749d59b782b144
Jody Biddle <Jody.Biddle@programmed.com.au>
2024-08-19 14:18:33 +1000

    hamburger menu and more reponsive design

--- components/component/footer.tsx
+++ components/component/footer.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="px-4 md:px-6 py-6 border-t border-[rgba(255,255,255,0.1)]">
-      <div className="container flex items-center justify-between">
+    <footer className="border-t border-[rgba(255,255,255,0.1)] px-4 py-6 md:px-6">
+      <div className="container flex flex-col space-y-2 xl:flex-row xl:items-center xl:justify-between xl:space-y-0">
         <p className="text-sm text-[rgba(255,255,255,0.6)]">
           &copy; 2024 Programmed Health Professionals. All rights reserved.
         </p>
 
-        <div className="flex items-center gap-4">
-          <p className="text-[rgba(255,255,255,0.8)] text-sm">
+        <div className="flex flex-col xl:flex-row">
+          <p className="text-sm text-[rgba(255,255,255,0.8)]">
             Address: 47 Burswood Road Burswood WA, 6100
           </p>
           <Link
-            className="text-[rgba(255,255,255,0.8)] text-sm hover:text-[rgb(132,189,0)]"
+            className="text-sm text-[rgba(255,255,255,0.8)] hover:text-[rgb(132,189,0)]"
             href="tel:13 11 48"
           >
             Telephone: 13 10 95
--- components/component/mobile-menu.tsx
+++ components/component/mobile-menu.tsx
@@ -3,30 +3,19 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useState } from "react";
-import MobileMenu from "./mobile-menu";
+import { Menu } from "lucide-react";
 
-export default function Header() {
+export default function MobileMenu() {
   const [showContacts, setShowContacts] = useState(false);
+  const [open, setOpen] = useState(false);
   return (
-    <header className="border-b border-[rgba(255,255,255,0.1)] px-4 py-4 md:px-6">
-      <div className="container flex items-center justify-between">
-        <div className="flex items-center space-x-2">
-          <MobileMenu />
-          <Link
-            href="#"
-            className="flex items-center gap-2 pl-5 sm:p-0"
-            prefetch={false}
-          >
-            <Image src="/favicon.png" alt="logo" width={35} height={35} />
-            <h1 className="invisible hidden text-xl font-medium italic lg:visible lg:block">
-              Programmed Health Professionals
-            </h1>
-          </Link>
-        </div>
-        <nav className="hidden items-center gap-4 sm:flex">
+    <div className="fixed left-3 top-6 z-10 flex flex-col bg-[rgb(12,35,64)] sm:hidden">
+      <Menu onClick={() => setOpen(!open)} />
+      {open && (
+        <nav className="fixed top-14 flex flex-col space-y-4 rounded-lg border border-[rgba(255,255,255,0.1)] bg-[rgb(12,35,64)] px-3 py-2">
           <Link
             href="/"
-            className="text-sm font-medium hover:text-[rgb(132,189,0)]"
+            className="pt-2 text-sm font-medium hover:text-[rgb(132,189,0)]"
             prefetch={false}
           >
             Home
@@ -53,7 +42,7 @@ export default function Header() {
               Contact Us
             </h4>
             {showContacts && (
-              <div className="absolute top-10 z-10 mt-2 w-[400px] rounded-md bg-[rgb(12,35,64)] py-2 shadow-lg">
+              <div className="absolute left-24 top-32 z-10 mt-2 w-[300px] rounded-md border border-[rgba(255,255,255,0.1)] bg-[rgb(12,35,64)] py-2 shadow-lg">
                 <div className="grid gap-2 px-4 py-3">
                   <div className="grid gap-4">
                     <div>
@@ -69,9 +58,9 @@ export default function Header() {
                       <h5 className="text-sm font-medium">General Enquiries</h5>
                       <Link
                         className="text-sm text-[rgba(255,255,255,0.8)]"
-                        href="mailto:info@programmedhealthprofessionals.com.au"
+                        href="mailto:info@programmed.com.au"
                       >
-                        info@programmedhealthprofessionals.com.au
+                        info@programmed.com.au
                       </Link>
                     </div>
                     <div>
@@ -80,7 +69,7 @@ export default function Header() {
                         className="text-sm text-[rgba(255,255,255,0.8)]"
                         href="mailto:payroll@programmedhealthprofessionals.com.au"
                       >
-                        payroll@programmedhealthprofessionals.com.au
+                        payroll@programmed.com.au
                       </Link>
                     </div>
                     <div>
@@ -91,7 +80,7 @@ export default function Header() {
                         className="text-sm text-[rgba(255,255,255,0.8)]"
                         href="registration@programmedhealthprofessionals.com.au"
                       >
-                        registration@programmedhealthprofessionals.com.au
+                        registration@programmed.com.au
                       </Link>
                     </div>
                     <div>
@@ -100,7 +89,7 @@ export default function Header() {
                         className="text-sm text-[rgba(255,255,255,0.8)]"
                         href="work@programmedhealthprofessionals.com.au"
                       >
-                        work@programmedhealthprofessionals.com.au
+                        work@programmedhealth.com.au
                       </Link>
                     </div>
                   </div>
@@ -114,24 +103,12 @@ export default function Header() {
                         Level 32, Tower 2, 727 Collins Street Melbourne VIC,
                         3008
                       </p>
-                      <Link
-                        className="text-sm text-[rgba(255,255,255,0.8)]"
-                        href="tel:13 11 48"
-                      >
-                        13 10 95
-                      </Link>
                     </div>
                     <div>
                       <h6 className="text-sm font-medium">Western Australia</h6>
                       <p className="text-sm text-[rgba(255,255,255,0.8)]">
                         47 Burswood Road Burswood WA, 6100
                       </p>
-                      <Link
-                        className="text-sm text-[rgba(255,255,255,0.8)]"
-                        href="tel:13 11 48"
-                      >
-                        13 10 95
-                      </Link>
                     </div>
                     <div>
                       <h6 className="text-sm font-medium">South Australia</h6>
@@ -144,36 +121,18 @@ export default function Header() {
                       <p className="text-sm text-[rgba(255,255,255,0.8)]">
                         Level 3, 200 Creek Street Spring Hill QLD, 4000
                       </p>
-                      <Link
-                        className="text-sm text-[rgba(255,255,255,0.8)]"
-                        href="tel:131095"
-                      >
-                        13 10 95
-                      </Link>
                     </div>
                     <div>
                       <h6 className="text-sm font-medium">New South Wales</h6>
                       <p className="text-sm text-[rgba(255,255,255,0.8)]">
                         Level 1, Building 2B, 2 Lord Street Botany NSW, 2019
                       </p>
-                      <Link
-                        className="text-sm text-[rgba(255,255,255,0.8)]"
-                        href="tel:131095"
-                      >
-                        13 10 95
-                      </Link>
                     </div>
                     <div>
                       <h6 className="text-sm font-medium">New South Wales</h6>
                       <p className="text-sm text-[rgba(255,255,255,0.8)]">
                         Level 6, 111 Phillip Street Parramatta NSW, 2150
                       </p>
-                      <Link
-                        className="text-sm text-[rgba(255,255,255,0.8)]"
-                        href="tel:131095"
-                      >
-                        13 10 95
-                      </Link>
                     </div>
                   </div>
                 </div>
@@ -181,19 +140,7 @@ export default function Header() {
             )}
           </div>
         </nav>
-        <Button
-          // variant="solid"
-          className="rounded-md bg-[rgb(132,189,0)] px-6 py-2 font-medium text-[rgb(12,35,64)] hover:bg-[rgb(106,152,0)]"
-        >
-          <Link
-            target="_blank"
-            href="https://php.fasttrack360.com.au/FastTrack.Web.Questionnaire/(S(fuzpugirjne1siuiax1e5yi4))/Questionnaire.page?Type=SbJobOrder&Reference=200000488&Source=Seek"
-            prefetch={false}
-          >
-            Apply Now
-          </Link>
-        </Button>
-      </div>
-    </header>
+      )}
+    </div>
   );
 }
